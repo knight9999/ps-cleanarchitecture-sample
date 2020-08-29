9bd41865dbba4c0899c1bac9576582467857dd1d
knaito <knaito@asial.co.jp>
2020-08-29 21:07:59 +0900

    feat: add showCUser

--- src/Domain/User.purs
+++ src/Domain/User.purs
@@ -16,16 +16,8 @@ type UserType =
 
 data User = User UserType
 
-instance showUser :: Show User where
-  show (User user) =
-    "User { id:" <> (toStringAs decimal (fromMaybe 0 user.id)) 
-    <> ", firstName: '" <> user.firstName <> "'"
-    <> ", lastName: '" <> user.lastName <> "' }"
-
--- instance readUser :: ReadForeign User where
---   readImpl text = do
---     (user :: UserType) <- readImpl text
---     pure $ User user
-
--- instance writeUser :: WriteForeign User where
---   writeImpl (User user) = writeImpl user
+-- instance showUser :: Show User where
+--   show (User user) =
+--     "User { id:" <> (toStringAs decimal (fromMaybe 0 user.id)) 
+--     <> ", firstName: '" <> user.firstName <> "'"
+--     <> ", lastName: '" <> user.lastName <> "' }"
--- src/Interfaces/Controllers/CUser.purs
+++ src/Interfaces/Controllers/CUser.purs
@@ -4,6 +4,8 @@ module Interfaces.Controllers.CUser
 
 import Prelude
 import Simple.JSON (class ReadForeign, readImpl, class WriteForeign, writeImpl)
+import Data.Int (decimal, toStringAs)
+import Data.Maybe (Maybe, fromMaybe)
 
 import Domain.User
 
@@ -18,3 +20,8 @@ instance writeCUser :: WriteForeign CUser where
   writeImpl (CUser (User user)) = writeImpl user
 
 
+instance showCUser :: Show CUser where
+  show (CUser (User user)) =
+    "User { id:" <> (toStringAs decimal (fromMaybe 0 user.id)) 
+    <> ", firstName: '" <> user.firstName <> "'"
+    <> ", lastName: '" <> user.lastName <> "' }"
